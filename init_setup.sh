9f19589443a306adeb25cbf6849a4636b76725af
Prashant-Baliyan <pbaliyan1992@gmail.com>
2022-12-23 20:20:38 +0530

    Basic Setup ZDone

--- init_setup.sh
+++ init_setup.sh
@@ -5,4 +5,6 @@ echo [$(date)]: "activating the environment"
 source activate ./env
 echo [$(date)]: "installing the dev requirements" 
 pip install -r requirements_dev.txt
+echo [$(date)]: "installing pytorch version 1.8.1" 
+conda install pytorch==1.8.1 cudatoolkit=11.3 -c pytorch -c conda-forge
 echo [$(date)]: "END"
